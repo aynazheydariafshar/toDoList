23b5f54edc48e3dd04059a0ad2b7161286809265
aynaz <aynaz@arianalabs.io>
2023-08-14 21:09:31 +0330

    FEAT:add title on page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,16 @@ import { GiCheckMark } from "react-icons/gi";
 
 export default function Home() {
   return (
-    <div className="flex justify-start text-[34px] items-center">
-      <GiCheckMark />
-      <h1 className="font-bold ">Task List</h1>
+    <div className="py-[70px] px-[120px]">
+      <div className="flex justify-start text-[34px] items-center">
+        <GiCheckMark />
+        <h1 className="font-bold mx-1">Task List</h1>
+      </div>
+      <p className="mt-[15px]">
+        Break your life to simple tasks to get things done!
+        <br /> Does not matter how many tasks you done, It’s important to break
+        to small tasks and be on progress.
+      </p>
     </div>
   );
 }
