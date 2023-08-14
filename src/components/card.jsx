12fb54c7b53f11477d55d84d3356f6d7f099a625
aynaz <aynaz@arianalabs.io>
2023-08-14 22:49:03 +0330

    FEAT:add number of task props

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 
-export default function Card({ title, icon = "", bgColorCard, titleColor }) {
+export default function Card({
+  title,
+  icon = "",
+  bgColorCard,
+  titleColor,
+  numberOfList,
+  colorCaption,
+  children
+}) {
   return (
     <div
       style={{ backgroundColor: bgColorCard }}
       className="w-[340px] h-[700px] rounded-[10px] px-[20px] pt-[20px] pb-[30px]"
     >
-      <div>
+      <div className="flex justify-between items-center">
         <p style={{ color: titleColor }} className="font-bold">
           {title} {icon}
         </p>
+        <p
+          style={{ color: colorCaption }}
+          className="text-[12px]"
+        >{`${numberOfList} Tasks`}</p>
       </div>
+      {children}
     </div>
   );
 }
